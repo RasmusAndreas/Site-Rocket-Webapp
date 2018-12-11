9a25cfe2d3b0041de6531a4ef85f31dc3ea94f5d
Rasmus Andreas Nielsen <>
2018-12-11 08:39:16 +0100

    More adding websites flow work

--- src/components/AddNewStepFour.vue
+++ src/components/AddNewStepFour.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
-        Hej 4
-        <v-btn @click="next">Next step</v-btn>
+        <h1 class="header-caps">INTEGRATION</h1>
+        <p>Use the snippet below to integrate the monitoring on your website.</p>
+        <v-textarea
+            readonly
+            :rows="13"
+            class="snippet"
+            name="input-7-1"
+            label="Integration snippet"
+            value="<!-- Start Site-Rocket Tracking Pixel -->
+    <script>window.onload=function(){var loadTime = window.performance.timing.domComplete-window.performance.timing.navigationStart;var websiteID='3';var apiKey = '241XeOQLeSnxGUKx3AbOr4iedpQvd0dsith1543237346';var pixelUrl='http://sovid.dk/scraper.php';document.getElementById('siterocket').src=pixelUrl+'?website='+websiteID+'&apikey='+apiKey+'&loadtime='+loadTime;}</script>
+    <img style='opacity: 0;' id='siterocket' />
+    <!-- End Site-Rocket Tracking Pixel -->"
+        ></v-textarea>
+        <p>The snippet has to be inserted before the &#60;/body> on all pages that needs to be tracked.</p>
+        <v-btn @click="next">Done</v-btn>
     </div>
 </template>
 
