3d427d2daa5b81bd57c353e75aae870a7e38257b
Rasmus Andreas Nielsen <>
2018-12-12 12:38:10 +0100

    Changed website to show conditionally.

--- src/views/Website.vue
+++ src/views/Website.vue
@@ -1,52 +1,54 @@
 <template>
     <div class="website">
-        <div class="website__header">
-            <div class="website__header__text">{{ websitedata.websiteName }}</div><div class="website__header__fadedtext">({{ websitedata.domain }})</div>
-        </div>
-        <v-tabs
-        v-model="active"
-        slider-color="grey">
-            <v-tab ripple>
-                Uptime
-            </v-tab>
-            <v-tab-item>
-                <v-card flat>
-                    <v-card-text>Uptime</v-card-text>
-                </v-card>
-            </v-tab-item>
-
-            <v-tab ripple>
-                SEO
-            </v-tab>
-            <v-tab-item>
-                <v-card flat>
-                    <v-card-text>SEO</v-card-text>
-                </v-card>
-            </v-tab-item>
-
-            <v-tab ripple>
-                Loadspeed
-            </v-tab>
-            <v-tab-item>
-                <v-card flat class="website-card" v-if="websitedata">
-                    <loadspeed-quick-insights :urls="websitedata.urls" header="Quick insight"/>
-                    <loadspeed-slowest-pages :urls="websitedata.urls" header="Slowest pages"/>
-                    <loadspeed-overview :urls="websitedata.urls" header="Loadspeed overview"/>
-                </v-card>
-            </v-tab-item>
-
-            <v-tab ripple>
-                Report
-            </v-tab>
-            <v-tab-item>
-                <v-card flat>
-                    <v-card-text>Report</v-card-text>
-                </v-card>
-            </v-tab-item>
-            <div class="settingsbutton" @click="navigateTo('/website/'+ $route.params.id +'/settings')">
-                <v-icon>settings</v-icon>
+        <div v-if="websitedata">
+            <div class="website__header">
+                <div class="website__header__text">{{ websitedata.websiteName }}</div><div class="website__header__fadedtext">({{ websitedata.domain }})</div>
             </div>
-        </v-tabs>
+            <v-tabs
+            v-model="active"
+            slider-color="grey">
+                <v-tab ripple>
+                    Uptime
+                </v-tab>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>Uptime</v-card-text>
+                    </v-card>
+                </v-tab-item>
+
+                <v-tab ripple>
+                    SEO
+                </v-tab>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>SEO</v-card-text>
+                    </v-card>
+                </v-tab-item>
+
+                <v-tab ripple>
+                    Loadspeed
+                </v-tab>
+                <v-tab-item>
+                    <v-card flat class="website-card" v-if="websitedata">
+                        <loadspeed-quick-insights :urls="websitedata.urls" header="Quick insight"/>
+                        <loadspeed-slowest-pages :urls="websitedata.urls" header="Slowest pages"/>
+                        <loadspeed-overview :urls="websitedata.urls" header="Loadspeed overview"/>
+                    </v-card>
+                </v-tab-item>
+
+                <v-tab ripple>
+                    Report
+                </v-tab>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>Report</v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <div class="settingsbutton" @click="navigateTo('/website/'+ $route.params.id +'/settings')">
+                    <v-icon>settings</v-icon>
+                </div>
+            </v-tabs>
+        </div>
     </div>
 </template>
 
